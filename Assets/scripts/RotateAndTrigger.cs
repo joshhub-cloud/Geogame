a8e54cac956c0647e86b3e98a787125d5b9fd48f
joshhub-cloud <joshua.idom.7e7@itb.cat>
2025-02-20 20:27:25 +0100

    Fixed Coin thing (I just had to add RigidBody smh)

--- Assets/scripts/RotateAndTrigger.cs
+++ Assets/scripts/RotateAndTrigger.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class RotateAndTrigger : MonoBehaviour
 {
     public Transform target; // The object to rotate around
-    public float speed = 10f; // Rotation speed
+    public float speed = 50f; // Rotation speed
     public Vector3 axis = Vector3.up; // Rotation axis
 
     public GameObject InactiveObject;
