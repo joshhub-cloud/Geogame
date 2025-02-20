42e3f85b32b420fb163b65d5cebbb5d16be3703a
joshhub-cloud <joshua.idom.7e7@itb.cat>
2025-02-20 20:32:47 +0100

    Update (Insert Number)
    Just playing around with the RayCast now trying to figure how to make it work.

--- Assets/scripts/Raycast_Script.cs
+++ Assets/scripts/Raycast_Script.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Raycast_Script : MonoBehaviour
 {
     private RaycastHit hit;
+    public float rayDistance = 10f; // Length of the Ray
+    public Color rayColor = Color.red; // Color of the Ray
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = transform.forward;
+        Debug.DrawRay(transform.position, direction * rayDistance, rayColor);
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
